192a9e4e9b24ca1efa4a8b690830fe467d158d66
christofer-bernardo <christofer.bernardo@autoglass.com.br>
2023-06-13 16:51:47 -0300

    End of Section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext context;
 
@@ -16,15 +16,18 @@ namespace API.Controllers
             this.context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
             var users = await context.Users.ToListAsync();
 
             return users;
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id){
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
             return await context.Users.FindAsync(id);
         }
     }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,12 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddCors();
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -20,6 +16,9 @@ var app = builder.Build();
 app.UseCors(builder => builder.AllowAnyHeader()
                               .AllowAnyMethod()
                               .WithOrigins("http://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
